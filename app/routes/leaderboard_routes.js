7630b8b1de35e96e75ae01648fc1b7ae638f856a
Ryan Sperzel <ryansperzel@gmail.com>
2018-10-28 01:45:09 -0400

    Added read functionality as '/leaderboards/:id' route

--- app/routes/leaderboard_routes.js
+++ app/routes/leaderboard_routes.js
@@ -1,4 +1,20 @@
+const ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app, db) {
+
+  app.get('/leaderboards/:id', (req, res) => {
+    console.log(req.params.id)
+    const id = req.params.id
+    const details = { _id: new ObjectID(id)}
+    db.db().collection('leaderboard').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ err: 'An error has occurred' })
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
   app.post('/leaderboard', (req, res) => {
     const board = { text: req.body.body, title: req.body.title }
     db.db().collection('leaderboard').insert(board, (err, result) => {
