f83553aca5fd93f88597276741b6122628b5e3a7
Ryan Sperzel <ryansperzel@gmail.com>
2018-10-28 01:59:13 -0400

    Added update functionality as '/leaderboards/:id' route

--- app/routes/leaderboard_routes.js
+++ app/routes/leaderboard_routes.js
@@ -3,7 +3,6 @@ const ObjectID = require('mongodb').ObjectID
 module.exports = function(app, db) {
 
   app.get('/leaderboards/:id', (req, res) => {
-    console.log(req.params.id)
     const id = req.params.id
     const details = { _id: new ObjectID(id)}
     db.db().collection('leaderboard').findOne(details, (err, item) => {
@@ -16,7 +15,6 @@ module.exports = function(app, db) {
   })
 
   app.delete('/leaderboards/:id', (req, res) => {
-    console.log(req.params.id)
     const id = req.params.id
     const details = { _id: new ObjectID(id)}
     db.db().collection('leaderboard').remove(details, (err) => {
@@ -24,6 +22,19 @@ module.exports = function(app, db) {
         res.send({ err: 'An error has occurred' })
       } else {
         res.send(`Leaderboard ${id} successfully deleted`)
+      }
+    })
+  })
+
+  app.put('/leaderboards/:id', (req, res) => {
+    const id = req.params.id
+    const details = { _id: new ObjectID(id)}
+    const board = { text: req.body.body, title: req.body.title }
+    db.db().collection('leaderboard').update(details, board, (err, item) => {
+      if (err) {
+        res.send({ err: 'An error has occurred' })
+      } else {
+        res.send(item)
       }
     })
   })
